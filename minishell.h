75a3c05b619a74684b95c0382da58df6f6ad903a
syr-charles <charles.dana@hec.edu>
2020-11-12 11:30:45 +0100

    Added minishell.h

---
+++ minishell.h
@@ -0,0 +1,17 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# define BUFFER_SIZE 100
+# include <errno.h>
+
+int		ft_gnl(int fd, char **line);
+char	**ft_split(char const *line, char sep);
+char	*ft_strjoin(char const *left, char const *right);
+#endif
